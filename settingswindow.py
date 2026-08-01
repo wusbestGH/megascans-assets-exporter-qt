5664a2a2464e7659878a9f9f49ff25849db6bb0c
wusbestGH <wusbestmail711@gmail.com>
2026-08-02 01:58:02 +0300

    add license to code files

--- settingswindow.py
+++ settingswindow.py
@@ -1,9 +1,24 @@
+# Copyright (C) 2026 wusbestGH
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 from PySide6.QtWidgets import *
-import json
 
 class SettingsWindow(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
+        print("Settings window opened.")
         self.setWindowTitle("Settings")
         self.setFixedSize(300, 180)
 
